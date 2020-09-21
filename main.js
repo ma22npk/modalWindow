55ac9952b42eb328938f26af135ce86518e10417
Kota Matsumoto <gunjo64@icloud.com>
2020-09-21 20:55:39 +0900

    マスクとモーダルの表示と非表示

--- main.js
+++ main.js
@@ -9,4 +9,22 @@ const modal = document.getElementById('modal');
 const mask = document.getElementById('mask');
 
 
+open.addEventListener('click',()=>
+{
+modal.classList.remove('hidden');
+mask.classList.remove('hidden');
+
+});
+close.addEventListener('click',()=>
+{
+modal.classList.add('hidden');
+mask.classList.add('hidden');
+
+});
+mask.addEventListener('click',()=>
+{
+//closeをクリックした時と同じ処理を呼び出す
+close.click();
+});
+
 }
